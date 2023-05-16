8f2554e5dd219f8226b00bbd0166dd4dd904fd9d
Lance Johnson <lancejjohnson@gmail.com>
2023-05-16 08:07:20 -0400

    Add event handling code

--- lib/pento_web/live/promo_live.ex
+++ lib/pento_web/live/promo_live.ex
@@ -20,7 +20,38 @@ defmodule PentoWeb.PromoLive do
     assign(socket, :form, to_form(Promo.change_recipient(recipient)))
   end
 
-  # def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
-  #   assign(socket, :changeset, IO.inspect(to_form(Promo.change_recipient(recipient)), label: "THE FORM" ))
-  # end
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      # phx won't display errors without an action on the changeset
+      |> Map.put(:action, :validate)
+
+    # NB: This is different than the text of the book because of book errata
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+
+  # TODO: I just made this up. it should include some kind of notification to
+  # the user confirming what happened. Likely should also redirect the user to
+  # a different page.
+  def handle_event(
+        "save",
+        %{"recipient" => recipient_params},
+        %{
+          assigns: %{recipient: recipient}
+        } = socket
+      ) do
+    {:ok, _} = Promo.send_promo(recipient, %{})
+
+    {
+      :noreply,
+      socket
+      |> assign_recipient()
+      |> assign_changeset()
+    }
+  end
 end
