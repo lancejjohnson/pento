fe1a1076a4039c1862f7ec9091e241f81b897a20
Lance Johnson <lancejjohnson@gmail.com>
2023-06-25 15:31:24 -0400

    Allow file image uploads

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -32,6 +32,15 @@ defmodule PentoWeb.ProductLive.FormComponent do
           <.button phx-disable-with="Saving...">Save Product</.button>
         </:actions>
       </.simple_form>
+      <%= for image <- @uploads.image.entries do %>
+        <%= for err <- upload_errors(@uploads.image, image) do %>
+          <.error><%= err %></.error>
+        <% end %>
+        <div class="mt-4">
+          <.live_img_preview entry={image} width="60" />
+        </div>
+        <progress value={image.progress} max="100" />
+      <% end %>
     </div>
     """
   end
@@ -54,6 +63,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
 
   @impl true
   def handle_event("validate", %{"product" => product_params}, socket) do
+    IO.inspect(product_params, label: "handle_event/validate")
+
     changeset =
       socket.assigns.product
       |> Catalog.change_product(product_params)
@@ -63,10 +74,14 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   def handle_event("save", %{"product" => product_params}, socket) do
+    IO.inspect(product_params, label: "handle_event/save")
+
     save_product(socket, socket.assigns.action, product_params)
   end
 
-  defp save_product(socket, :edit, product_params) do
+  defp save_product(socket, :edit, params) do
+    product_params = params_with_image(socket, params)
+
     case Catalog.update_product(socket.assigns.product, product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -81,7 +96,10 @@ defmodule PentoWeb.ProductLive.FormComponent do
     end
   end
 
-  defp save_product(socket, :new, product_params) do
+  defp save_product(socket, :new, params) do
+    product_params = params_with_image(socket, params)
+    IO.inspect(product_params, label: "save_product")
+
     case Catalog.create_product(product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -101,4 +119,22 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+
+  defp params_with_image(socket, params) do
+    path =
+      socket
+      |> consume_uploaded_entries(:image, &upload_static_file/2)
+      |> List.first()
+
+    Map.put(params, "image_upload", path)
+  end
+
+  # NOTE: This is a side effect and shouldn't be handled in the form component code.
+  defp upload_static_file(%{path: path}, _entry) do
+    # Plug in your production image file persistence implementation here!
+    filename = Path.basename(path)
+    dest = Path.join("priv/static/images", filename)
+    File.cp!(path, dest)
+    {:ok, ~p"/images/#{filename}"}
+  end
 end
